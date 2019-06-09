f1d66e342447164de88e6ec5c38af2152ea663e1
henryhxyao <yaohongxiang543@sina.com>
2019-06-09 22:36:30 +0800

    remove build and devel

--- ros/devel/share/styx_msgs/cmake/styx_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(styx_msgs_MESSAGE_FILES "/home/henry/workspace/CarND-Capstone/ros/src/styx_msgs/msg/TrafficLight.msg;/home/henry/workspace/CarND-Capstone/ros/src/styx_msgs/msg/TrafficLightArray.msg;/home/henry/workspace/CarND-Capstone/ros/src/styx_msgs/msg/Waypoint.msg;/home/henry/workspace/CarND-Capstone/ros/src/styx_msgs/msg/Lane.msg")
-set(styx_msgs_SERVICE_FILES "")
